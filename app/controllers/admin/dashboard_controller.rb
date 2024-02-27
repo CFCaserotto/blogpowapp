8a8a6ae338ff1331f781775b0ce506a198ea846a
CFCaserotto <carlos.caserotto@gamil.com>
2024-02-27 14:15:24 -0300

    Add a dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < Admin::BaseController
     layout 'admin/base'
 
     def index
-        @users = User.all.includes(:blog)
+        @users = User.all.includes(:blogs)
     end
 
 end
